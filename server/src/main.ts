c9b1280e37d628f6983b15b865246e95f16f08ff
Agustin <deadmanwonderland171@gmail.com>
2025-07-27 09:21:28 -0300

    render index.html

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // O "*" para testing :p
+    origin: process.env.FRONTEND_URL || '*',
     credentials: true,
   });
 
-  await app.listen(3000);
+  app.useStaticAssets(join(__dirname, '..', 'client'));
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.get('/', (_, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'index.html'));
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
 void bootstrap();
